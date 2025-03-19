2dcd0b235a25b7834143ec1fed34362d5046abce
snavedee <snaveford@gmail.com>
2025-03-19 13:31:32 +0300

    Configured static files

--- mr_makeit/settings.py
+++ mr_makeit/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # If you have a local static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
