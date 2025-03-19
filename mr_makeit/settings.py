501624ffe9b157e80b807fd70d9bc4b3762c023a
snavedee <snaveford@gmail.com>
2025-03-19 13:42:08 +0300

    Configured static files with WhiteNoise

--- mr_makeit/settings.py
+++ mr_makeit/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # If you have a local static folder
-]
+# Ensure Render serves static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Middleware for static files on Render
+MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
