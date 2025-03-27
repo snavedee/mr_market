67781e65ed5d8f7bbdd88ec6a235c347b7f07092
snavedee <snaveford@gmail.com>
2025-03-27 14:33:22 +0300

    Fix: Remove PyScript and add request for sharing

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,4 @@ def post_list(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'request': request})  # <-- Added 'request'
