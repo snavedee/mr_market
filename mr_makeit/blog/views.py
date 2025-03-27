b0fcf1de2b7d2d60f430d5b24e93ebd0b81c6f96
snavedee <snaveford@gmail.com>
2025-03-27 13:59:50 +0300

    Updated navbar with active state fix and ensured Home appears twice

--- mr_makeit/blog/views.py
+++ mr_makeit/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.http import urlencode
 from .models import Post
 
 def home(request):
     return render(request, 'mr_makeit/home.html')
-
 
 def post_list(request):
     posts = Post.objects.all().order_by('-pub_date')
@@ -11,4 +11,11 @@ def post_list(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    post_url = request.build_absolute_uri()  # Get full URL of the post
+    twitter_share_url = f"https://twitter.com/intent/tweet?{urlencode({'text': post.title, 'url': post_url})}"
+    
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'post_url': post_url,
+        'twitter_share_url': twitter_share_url,
+    })
